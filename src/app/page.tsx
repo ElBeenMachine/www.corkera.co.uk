aac13a88d2a8686ec25295dda9cd1cdef86e9013
Ollie Beenham <ollie@beenhamow.co.uk>
2024-08-01 17:51:17 +0100

    Updated metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ const title = "Alice Corker - My Story";
 const description = "I'm just a girl. Just an ordinary girl, who has dedicated her life to writing. My name is Alice, and this is my story.";
 
 export const metadata: Metadata = {
+    metadataBase: new URL("https://corkera.co.uk"),
     title,
     description,
     openGraph: {
